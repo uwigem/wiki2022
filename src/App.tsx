99ddd231d173950824be6fd5c2732decd6393a37
William Kwok <wkwok16@uw.edu>
2019-09-12 23:08:35 -0700

    Updated Project Title and Abstract

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,10 @@ import "./App.css";
 window.addEventListener("unload", function () { });
 
 type AppProps = {
-    IEOREDGE: boolean,
-    currYear: number,
-    firebase?: any,
-    ContentEditor: React.FC<ContentEditorProps>
+	IEOREDGE: boolean,
+	currYear: number,
+	firebase?: any,
+	ContentEditor: React.FC<ContentEditorProps>
 }
 const debugURL = "/MAIN_PAGE";
 
@@ -37,30 +37,30 @@ const debugURL = "/MAIN_PAGE";
  * July 17, 2019
  */
 const App: React.FC<AppProps> = ({ IEOREDGE, currYear, firebase, ContentEditor }) => {
-    const [loading, setLoading] = useState<boolean>(true);
-    const [contentData, setContentData] = useState<ContentData>(Data.getContentData());
-    const [pageTitle, setPageTitle] = useState<string>(debugURL)
-    const [debugMode, setDebugMode] = useState<boolean>(true);
-    const windowWidth = useWindowWidth();
+	const [loading, setLoading] = useState<boolean>(true);
+	const [contentData, setContentData] = useState<ContentData>(Data.getContentData());
+	const [pageTitle, setPageTitle] = useState<string>(debugURL)
+	const [debugMode, setDebugMode] = useState<boolean>(true);
+	const windowWidth = useWindowWidth();
 
-    let name = `https://${currYear}.igem.org/Team:Washington`;
-    let imgsToPrefetch: string[] = Data.getImgsToPrefetch();
-    let imgsLoaded = 0;
+	let name = `https://${currYear}.igem.org/Team:Washington`;
+	let imgsToPrefetch: string[] = Data.getImgsToPrefetch();
+	let imgsLoaded = 0;
 
-    const displayConstants = Data.getDisplayConstants(pageTitle);
-    const theme = createMuiTheme({
-        palette: {
-            primary: {
-                main: displayConstants.primaryColor as string
-            },
-            secondary: {
-                main: displayConstants.secondaryColor as string
-            }
-        },
-        typography: {
-            fontFamily: "Raleway"
-        }
-    });
+	const displayConstants = Data.getDisplayConstants(pageTitle);
+	const theme = createMuiTheme({
+		palette: {
+			primary: {
+				main: displayConstants.primaryColor as string
+			},
+			secondary: {
+				main: displayConstants.secondaryColor as string
+			}
+		},
+		typography: {
+			fontFamily: "Raleway"
+		}
+	});
 
     /**
      * Upon component mount, this effect determines if it is currently on the live website or not.
@@ -69,56 +69,56 @@ const App: React.FC<AppProps> = ({ IEOREDGE, currYear, firebase, ContentEditor }
      * 
      * eg "2019.igem.org/Team:Washington/Design" -> "/Design"
      */
-    useEffect(() => {
+	useEffect(() => {
         /**
          * Prefetch images will grab images to load in the background after the page has loaded
          */
-        const prefetchImagesWaveTwo = () => {
+		const prefetchImagesWaveTwo = () => {
 
-        }
+		}
 
         /**
          * Prefetch images will grab images to load in the background after the page has loaded
          */
-        const prefetchImages = () => {
-            imgsToPrefetch.forEach((imgURL: string) => {
-                const img = new Image();
-                img.src = imgURL;
-                img.onload = () => {
-                    imgsLoaded++;
-                    if (imgsLoaded === imgsToPrefetch.length) {
-                        prefetchImagesWaveTwo();
-                    }
-                }
-            })
-        }
-        prefetchImages();
-        let splitTitle = window.location.href.split("igem");
-        if (splitTitle.length === 2) {
-            setPageTitle(splitTitle[1].split("Team:Washington")[1]);
-            setDebugMode(false);
-        } else {
-            DebugFonts(); // Set fonts in body
-        }
-        setLoading(false);
-        let dataRef: firebase.database.Reference | null = null;
-        if (firebase) {
-            dataRef = firebase.database().ref(`/${currYear}/ContentData`);
-            if (dataRef) {
-                dataRef.on("value", (snap) => {
-                    setContentData(snap.val() as ContentData);
-                });
-            }
+		const prefetchImages = () => {
+			imgsToPrefetch.forEach((imgURL: string) => {
+				const img = new Image();
+				img.src = imgURL;
+				img.onload = () => {
+					imgsLoaded++;
+					if (imgsLoaded === imgsToPrefetch.length) {
+						prefetchImagesWaveTwo();
+					}
+				}
+			})
+		}
+		prefetchImages();
+		let splitTitle = window.location.href.split("igem");
+		if (splitTitle.length === 2) {
+			setPageTitle(splitTitle[1].split("Team:Washington")[1]);
+			setDebugMode(false);
+		} else {
+			DebugFonts(); // Set fonts in body
+		}
+		setLoading(false);
+		let dataRef: firebase.database.Reference | null = null;
+		if (firebase) {
+			dataRef = firebase.database().ref(`/${currYear}/ContentData`);
+			if (dataRef) {
+				dataRef.on("value", (snap) => {
+					setContentData(snap.val() as ContentData);
+				});
+			}
 
-            return () => {
-                if (dataRef) {
-                    dataRef.off();
-                }
-            }
-        } else {
-            setContentData(Data.getContentData())
-        }
-    }, [pageTitle, imgsLoaded, imgsToPrefetch, currYear, firebase]);
+			return () => {
+				if (dataRef) {
+					dataRef.off();
+				}
+			}
+		} else {
+			setContentData(Data.getContentData())
+		}
+	}, [pageTitle, imgsLoaded, imgsToPrefetch, currYear, firebase]);
 
     /**
      * Sets the loading state to true. This is used for in between pages. This function MUST be sent
@@ -137,62 +137,73 @@ const App: React.FC<AppProps> = ({ IEOREDGE, currYear, firebase, ContentEditor }
      *       )
      *   }
      */
-    const a = () => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 3000);
-    }
+	const a = () => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 3000);
+	}
 
-    return <div className="App">
-        <EnvironmentContext.Provider value={{ windowWidth, firebase }}>
-            <MuiThemeProvider theme={theme}>
-                {debugMode && <>
-                    <DebugHeader />
-                </>}
+	return <div className="App">
+		<EnvironmentContext.Provider value={{ windowWidth, firebase }}>
+			<MuiThemeProvider theme={theme}>
+				{debugMode && <>
+					<DebugHeader />
+				</>}
 
-                <CustomAppBar name={name} pageTitle={pageTitle} a={a} />
+				<CustomAppBar name={name} pageTitle={pageTitle} a={a} />
 
-                {!loading && <>
-                    <div className={`${debugMode ? "app-content-dev" : "app-content-real"}
+				{!loading && <>
+					<div className={`${debugMode ? "app-content-dev" : "app-content-real"}
                         ${windowWidth < 600 ? "mobile" : ""}`}>
-                        {/** Comment out this in final build */}
-                        {pageTitle === "/Editor" &&
-                            <ContentEditor
-                                contentData={contentData}
-                                currYear={currYear}
-                            />}
-                        {/*****************************************/}
-                        {pageTitle !== "/Editor" && (pageTitle === "" || pageTitle === "/Description") &&
-                            <div className="main-temporary">
-                                <h1 style={{ margin: "auto", textAlign: "center" }}>Project Inspiration and Description</h1>
-                                <p>
-                                    Our project focuses on developing technologies that help researchers to detect small molecules, which traditionally have been much more difficult to detect using biological assays than larger molecules. Our technologies revolve around chemically induced dimerization (CID), in which two proteins dimerize in the presence of a small molecule. This method has been previously used to control cell signaling, regulatory and metabolic pathways, and logic gates for biological computation in living mammalian cells. However, few naturally-occuring CID systems and their derivatives are currently available for research and therapeutic purposes. Creating a CID system with desired affinity and specificity for any given small molecule remains an unsolved problem for computational design and other protein engineering approaches. Solving it would open a wide range of possibilities in the development of diagnostic biosensors, high-precision therapeutics, and beyond.
-                                </p>
-                                <p>
-                                    We are using a novel strategy to select CID binders from a vast and diverse combinatorial antibody library, and applying them towards the development of a biosensor. We will specifically be developing a biosensor using these antibodies to detect cannabidiol (CBD), a compound derived from the hemp plant which has been used to treat epilepsy, anxiety, and chronic pain, although more research is needed to verify its effectiveness. We are working to engineer this biosensor both as a proof of concept and as a way to help further research into CBD.
-                                </p>
-                                <p>
-                                    We hope that the result of our project will benefit the scientific community by creating a generalizable bioengineered tool that can detect the presence of small molecules with a high degree of accuracy and efficiency, at a lower cost than other tools that have been traditionally used.
-                                </p>
-                                <p>
-                                    This project was inspired by work in the Gu Lab at the University of Washington and their work dealing with the creation of a CID system. We wanted to take what we learned from last year, build upon it, and package everything in a consumer friendly device to detect CBD. Furthermore, since our team has had experience with CID devices from our 2018 project that dealt with detecting anchor binders for cholecalciferol and artemisinin.
-                                </p>
-                            </div>}
+						{/** Comment out this in final build */}
+						{pageTitle === "/Editor" &&
+							<ContentEditor
+								contentData={contentData}
+								currYear={currYear}
+							/>}
+						{/*****************************************/}
+						{pageTitle !== "/Editor" && (pageTitle === "" || pageTitle === "/Description") &&
+							<div className="main-temporary">
+								{pageTitle !== '/Description' &&
+									<div>
+										<h1 style={{ margin: "auto", textAlign: "center" }}>Immunosense: Detecting small molecules using a chemically-induced dimerization system</h1>
 
-                        {/*****************************************/}
-                        {pageTitle !== "/Editor" && pageTitle !== "" &&
-                            <ContentView contentData={contentData} pageTitle={pageTitle} />
-                        }
-                    </div>
-                    <Footbar a={a} />
-                </>}
+										<h2 style={{ margin: "auto", textAlign: "center" }}>Abstract:</h2>
+										<p>
+											Although biosensors are commonly used to detect many different molecules of interest, they cannot effectively detect small hydrophobic molecules in biological systems. We propose combining chemically induced dimerization (CID), in which two proteins dimerize only in the presence of a ligand, with a traditional luciferase assay to create a biosensor that luminesces when the desired molecule is introduced. Using molecule-specific nanobodies, we can design the two CID binders to attach to a wide variety of small molecules, even those that are challenging for conventional biosensors to detect. Through its specificity and ability to bind to small or hydrophobic molecules, the CID system overcomes problems that other biosensors face. As a proof-of-concept, we implemented an in vivo CID biosensor to detect the presence of cannabidiol. With the nanobody CID system, we hope to introduce a novel biosensor that can detect a variety of important small molecules across research, biotechnology, and medicine.
+										</p>
+									</div>
+								}
 
-                {loading &&
-                    <LoadingScreen />}
-            </MuiThemeProvider>
-        </EnvironmentContext.Provider>
-    </div>
+								<h1 style={{ margin: "auto", textAlign: "center" }}>Project Inspiration and Description</h1>
+								<p>
+									Our project focuses on developing technologies that help researchers to detect small molecules, which traditionally have been much more difficult to detect using biological assays than larger molecules. Our technologies revolve around chemically induced dimerization (CID), in which two proteins dimerize in the presence of a small molecule. This method has been previously used to control cell signaling, regulatory and metabolic pathways, and logic gates for biological computation in living mammalian cells. However, few naturally-occuring CID systems and their derivatives are currently available for research and therapeutic purposes. Creating a CID system with desired affinity and specificity for any given small molecule remains an unsolved problem for computational design and other protein engineering approaches. Solving it would open a wide range of possibilities in the development of diagnostic biosensors, high-precision therapeutics, and beyond.
+                                </p>
+								<p>
+									We are using a novel strategy to select CID binders from a vast and diverse combinatorial antibody library, and applying them towards the development of a biosensor. We will specifically be developing a biosensor using these antibodies to detect cannabidiol (CBD), a compound derived from the hemp plant which has been used to treat epilepsy, anxiety, and chronic pain, although more research is needed to verify its effectiveness. We are working to engineer this biosensor both as a proof of concept and as a way to help further research into CBD.
+                                </p>
+								<p>
+									We hope that the result of our project will benefit the scientific community by creating a generalizable bioengineered tool that can detect the presence of small molecules with a high degree of accuracy and efficiency, at a lower cost than other tools that have been traditionally used.
+                                </p>
+								<p>
+									This project was inspired by work in the Gu Lab at the University of Washington and their work dealing with the creation of a CID system. We wanted to take what we learned from last year, build upon it, and package everything in a consumer friendly device to detect CBD. Furthermore, since our team has had experience with CID devices from our 2018 project that dealt with detecting anchor binders for cholecalciferol and artemisinin.
+                                </p>
+							</div>}
+
+						{/*****************************************/}
+						{pageTitle !== "/Editor" && pageTitle !== "" &&
+							<ContentView contentData={contentData} pageTitle={pageTitle} />
+						}
+					</div>
+					<Footbar a={a} />
+				</>}
+
+				{loading &&
+					<LoadingScreen />}
+			</MuiThemeProvider>
+		</EnvironmentContext.Provider>
+	</div>
 }
 
 
