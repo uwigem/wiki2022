770c3344132d215998f686272afe75a629cc86a0
zhouyifan0904 <zhouyf98@uw.edu>
2019-10-12 01:50:53 -0700

    Widget live edit (#34)
    * updated live edit

--- src/components/ContentEditor/WidgetEditor/WidgetEditor.tsx
+++ src/components/ContentEditor/WidgetEditor/WidgetEditor.tsx
@@ -4,12 +4,11 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import Button from '@material-ui/core/Button';
 import './WidgetEditor.css';
 import { WidgetTypes, ContentMapping } from '../../ContentMapping/ContentMapping';
-import { HistoryTypes } from '../../_debug/EditorHistory';
 import equal from 'deep-equal';
 import { EnvironmentContext } from '../../../contexts/EnvironmentContext/EnvironmentContext';
+import { WidgetLiveEdit } from '../WidgetLiveEdit/WidgetLiveEdit';
 
 type WidgetEditorProps = {
 	content: ContentSingularData | undefined,
@@ -58,10 +57,15 @@ export const WidgetEditor: React.FC<WidgetEditorProps> = ({ content, contentHash
 		{!editing && <>
 			<ContentWidget {...editedContent} />
 			<div>
-				<Button variant="contained" color="primary"
-					onClick={() => setEditing(true)}>Edit</Button>
-				<Button variant="contained" color="primary"
-					onClick={() => deleteWidget(contentHash)}>Delete</Button>
+				<WidgetLiveEdit
+					contentHash={contentHash}
+					currYear={currYear}
+					pageToEdit={pageToEdit}
+					user={user}
+					editing={false}
+					setEditing={setEditing}
+					editedContent={editedContent}
+					deleteWidget={deleteWidget} />
 			</div>
 		</>}
 
@@ -88,19 +92,15 @@ export const WidgetEditor: React.FC<WidgetEditorProps> = ({ content, contentHash
 					setEditedContentOnChange(keyToChange, valueToChange, editedContent, setEditedContent);
 				}} />
 			<div>
-				<Button variant="contained" color="primary"
-					onClick={async () => {
-						await firebase.database().ref(`${currYear}/ContentData/${pageToEdit}/content/${contentHash}`).set(editedContent);
-						await firebase.database().ref(`${currYear}/EditHistory/${pageToEdit}/${contentHash}`).push({
-							type: HistoryTypes.UPDATE,
-							timestamp: firebase.database.ServerValue.TIMESTAMP,
-							creator: (user && user.email) || "Unknown user",
-							content: editedContent
-						});
-						setEditing(false);
-					}}>
-					Save
-                </Button>
+				<WidgetLiveEdit
+					contentHash={contentHash}
+					currYear={currYear}
+					pageToEdit={pageToEdit}
+					user={user}
+					editing={true}
+					setEditing={setEditing}
+					editedContent={editedContent}
+					deleteWidget={deleteWidget} />
 			</div>
 		</>}
 	</div>
