5dab64c3950fa5a00fadb3ee7eeb6c164033bf99
diandremiguels <diandresabale@gmail.com>
2022-08-18 19:08:37 -0700

    finished text separator

--- src/components/ContentWidgets/TextSeparator/TextSeparator.tsx
+++ src/components/ContentWidgets/TextSeparator/TextSeparator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ContentSingularData } from '../../_data/ContentSingularData';
 import './TextSeparator.css';
 import {Grid, Divider} from "@material-ui/core";
+import { DEFAULT_COLOR, DEFAULT_SIZE } from './TextSeparatorConstants';
 /**
  * A separator widget with the given width, text, and color
  * separator_width  percentage width
@@ -13,24 +14,27 @@ import {Grid, Divider} from "@material-ui/core";
  * July 26, 2022
  */
 export const TextSeparator: React.FC<ContentSingularData> = ({
-	plainText_content
+	plainText_content,
+	text_separator_size,
+	text_separator_color
 }) => {
+	const { r, g, b } = text_separator_color || DEFAULT_COLOR;
+	const size = text_separator_size || DEFAULT_SIZE;
+	
+	// Current issues:
+	// - width doesn't change dynamically (always starts at specified default, and once edited goes to internal default value??)
+
 	// originally tried using a single divider and putting text in between but couldn't get it to work
 	// now, we have two dividers on either side and text in the middle, all separated by grids
-	// next step: make editing work
-	// what we want to change (in order of priority):
-	// 1. Text
-	// 2. Width
-	// 3. Color
 	return <div className="text-separator-container wi-content-wrapper">
 		<Grid container alignItems="center" spacing={3}>
-    <Grid item xs>
-      <Divider />
-    </Grid>
-    <Grid item>{plainText_content}</Grid>
-    <Grid item xs>
-      <Divider />
-    </Grid>
-  </Grid>
+    		<Grid item xs>
+      			<Divider style={{height: size + 'px', background: `rgb(${r},${g},${b})`}}/>
+    		</Grid>
+    			<Grid item>{plainText_content}</Grid>
+    		<Grid item xs>
+      			<Divider style={{height: size + 'px', background: `rgb(${r},${g},${b})`}}/>
+    		</Grid>
+  		</Grid>
 	</div>
 };
