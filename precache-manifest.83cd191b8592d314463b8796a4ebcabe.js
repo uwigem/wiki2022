28300b6ab7a9587706d5034919033ea2d805b9bc
MilkyTea123 <63531427+MilkyTea123@users.noreply.github.com>
2022-09-04 21:48:51 -0700

    Updates

---
+++ precache-manifest.83cd191b8592d314463b8796a4ebcabe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa90bc27e9d68551ffc0caa85c8ca4a",
+    "url": "/igem-wiki2022-editor/index.html"
+  },
+  {
+    "revision": "9f3d16536c572e78a5f1",
+    "url": "/igem-wiki2022-editor/static/css/main.css"
+  },
+  {
+    "revision": "9f3d16536c572e78a5f1",
+    "url": "/igem-wiki2022-editor/static/js/main.js"
+  },
+  {
+    "revision": "47a6bb091aa459578b95cd90574064e9",
+    "url": "/igem-wiki2022-editor/static/js/main.js.LICENSE.txt"
+  }
+]);
